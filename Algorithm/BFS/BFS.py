adac14767a57c01e84adfefc2c03687963c86e92
DrK <wldnro3141@gmail.com>
2019-12-11 15:21:00 +0900

    BFS 알고리즘 학습

---
+++ Algorithm/BFS/BFS.py
@@ -0,0 +1,29 @@
+graph = {
+    'A': ['B'],
+    'B': ['A', 'C', 'H'],
+    'C': ['B', 'D', 'G'],
+    'D': ['C', 'E'],
+    'E': ['D', 'F'],
+    'F': ['E'],
+    'G': ['D'],
+    'H': ['B', 'I', 'J', 'M'],
+    'I': ['H'],
+    'J': ['H', 'K'],
+    'K': ['J', 'L'],
+    'L': ['K'],
+    'M': ['H']
+}
+
+if __name__ == "__main__":
+    visit=list()
+    queue=list()
+    queue.append('A')
+
+    while queue:
+        flag=False
+        node=queue.pop(0)
+        if node not in visit:
+            print(node)
+            visit.append(node)
+            for i in graph[node]:
+                queue.extend(graph[node])
